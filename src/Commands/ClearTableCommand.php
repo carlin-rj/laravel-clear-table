ead209eb75cd457b09a981385c3c92615cfdea26
wanghaojie <814425737@qq.com>
2023-09-15 15:39:26 +0800

    feat: 初始化

--- src/Commands/ClearTableCommand.php
+++ src/Commands/ClearTableCommand.php
@@ -50,6 +50,11 @@ class ClearTableCommand extends Command
             $tableConfigs = [$tableConfig];
         }
 
+		if (empty($tableConfigs)) {
+			$this->error("not configure clear_tables");
+			return;
+		}
+
         foreach ($tableConfigs as $tableName => $tableConfig) {
             $dateRanges = generateDateRanges($interval, $unit, now()->subDays($startDay), now()->subDays($endDay));
             foreach ($dateRanges as $range) {
