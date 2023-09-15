a3e18b5d433a7de51affc8611228a914ade74765
wanghaojie <814425737@qq.com>
2023-09-15 16:04:04 +0800

    feat: 初始化

--- src/Commands/ClearTableCommand.php
+++ src/Commands/ClearTableCommand.php
@@ -8,89 +8,89 @@ use Illuminate\Support\Facades\DB;
 
 class ClearTableCommand extends Command
 {
-	/**
-	 * The name and signature of the console command.
-	 *
-	 * @var string
-	 */
-	protected $signature = 'clear-tables {startDay=40} {endDay=30} {interval=10} {unit=day} {--table=}';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'clear-tables {startDay=40} {endDay=30} {interval=10} {unit=day} {--table=}';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = '定时清理指定表数据';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '定时清理指定表数据';
 
-	/**
-	 * Execute the console command.
-	 */
-	public function handle()
-	{
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
 
-		$startDay = (int) $this->argument('startDay');
-		$endDay = (int) $this->argument('endDay');
-		$interval = (int) $this->argument('interval');
-		$unit = $this->argument('unit');
-		$specificTable = $this->option('table');
+        $startDay = (int) $this->argument('startDay');
+        $endDay = (int) $this->argument('endDay');
+        $interval = (int) $this->argument('interval');
+        $unit = $this->argument('unit');
+        $specificTable = $this->option('table');
 
-		if ($startDay < $endDay) {
-			$this->error('startDay必须大于endDay');
+        if ($startDay < $endDay) {
+            $this->error('startDay必须大于endDay');
 
-			return;
-		}
-		$tableConfigs = config('clear-tables');
-		if (isset($specificTable)) {
-			$tableConfig = $this->findTableConfig($specificTable, $tableConfigs);
-			if (empty($tableConfig)) {
-				$this->error("{$specificTable} not configure clear_tables");
+            return;
+        }
+        $tableConfigs = config('clear-tables');
+        if (isset($specificTable)) {
+            $tableConfig = $this->findTableConfig($specificTable, $tableConfigs);
+            if (empty($tableConfig)) {
+                $this->error("{$specificTable} not configure clear_tables");
 
-				return;
-			}
-			$tableConfigs = [$tableConfig];
-		}
+                return;
+            }
+            $tableConfigs = [$tableConfig];
+        }
 
-		if (empty($tableConfigs)) {
-			$this->error('not configure clear_tables');
+        if (empty($tableConfigs)) {
+            $this->error('not configure clear_tables');
 
-			return;
-		}
+            return;
+        }
 
-		foreach ($tableConfigs as $tableName => $tableConfig) {
-			$dateRanges = generateDateRanges($interval, $unit, now()->subDays($startDay), now()->subDays($endDay));
-			foreach ($dateRanges as $range) {
-				$totalDeletedForRange = 0;  // 初始化计数器，用于计算当前日期范围内的删除总数
-				/** @var Builder $query */
-				$query = $tableConfig['model']::query();
-				$tableConfig['conditions']($query);  // Apply conditions
-				$query->whereBetween($tableConfig['date_column'], [$range['start'], $range['end']])
-					->select('id')
-					->chunkById($tableConfig['chunk_size'] ?? 1000, function ($logs) use (&$totalDeletedForRange, $tableConfig) {
-						$idsToDelete = $logs->pluck('id')->toArray();
-						if (! empty($idsToDelete)) {
-							$deletedCount = $tableConfig['model']::query()->whereIn('id', $idsToDelete)->delete();
-							$totalDeletedForRange += $deletedCount;
-						}
-					});
-				$this->info("Deleted logs from {$range['start']} to {$range['end']}. Total: $totalDeletedForRange");
-			}
+        foreach ($tableConfigs as $tableName => $tableConfig) {
+            $dateRanges = generateDateRanges($interval, $unit, now()->subDays($startDay), now()->subDays($endDay));
+            foreach ($dateRanges as $range) {
+                $totalDeletedForRange = 0;  // 初始化计数器，用于计算当前日期范围内的删除总数
+                /** @var Builder $query */
+                $query = $tableConfig['model']::query();
+                $tableConfig['conditions']($query);  // Apply conditions
+                $query->whereBetween($tableConfig['date_column'], [$range['start'], $range['end']])
+                    ->select('id')
+                    ->chunkById($tableConfig['chunk_size'] ?? 1000, function ($logs) use (&$totalDeletedForRange, $tableConfig) {
+                        $idsToDelete = $logs->pluck('id')->toArray();
+                        if (! empty($idsToDelete)) {
+                            $deletedCount = $tableConfig['model']::query()->whereIn('id', $idsToDelete)->delete();
+                            $totalDeletedForRange += $deletedCount;
+                        }
+                    });
+                $this->info("Deleted logs from {$range['start']} to {$range['end']}. Total: $totalDeletedForRange");
+            }
 
-			// 优化表
-			if (! empty($tableConfig['is_optimized'])) {
-				DB::statement('OPTIMIZE TABLE '.$tableName);
-				$this->info("Optimized the {$tableName} table.");
-			}
-		}
-	}
+            // 优化表
+            if (! empty($tableConfig['is_optimized'])) {
+                DB::statement('OPTIMIZE TABLE '.$tableName);
+                $this->info("Optimized the {$tableName} table.");
+            }
+        }
+    }
 
-	private function findTableConfig(string $tableName, array $tableConfigs): ?array
-	{
-		foreach ($tableConfigs as $config) {
-			if ($tableName === (new $config['model'])->getTable()) {
-				return $config;
-			}
-		}
+    private function findTableConfig(string $tableName, array $tableConfigs): ?array
+    {
+        foreach ($tableConfigs as $config) {
+            if ($tableName === (new $config['model'])->getTable()) {
+                return $config;
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 }
