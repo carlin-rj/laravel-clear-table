b9cce080dff04e7283c832e217c8f7a4ae97f167
wanghaojie <814425737@qq.com>
2023-09-15 14:31:06 +0800

    feat: 初始化

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,20 +6,20 @@ use Mckue\LaravelClearTable\Commands\ClearTableCommand;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-	public function boot() {
-		if ($this->app->runningInConsole()) {
-			$this->commands([
-				ClearTableCommand::class,
-			]);
-		}
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ClearTableCommand::class,
+            ]);
+        }
 
-		$this->publishes([
-			__DIR__ . '/config/clear-table.php' => config_path('clear-table.php'),
-		], 'clear-table');
+        $this->publishes([
+            __DIR__.'/config/clear-table.php' => config_path('clear-table.php'),
+        ], 'clear-table');
 
-		$this->mergeConfigFrom(
-			__DIR__ . '/config/clear-table.php', 'clear-table'
-		);
-	}
-
+        $this->mergeConfigFrom(
+            __DIR__.'/config/clear-table.php', 'clear-table'
+        );
+    }
 }
--- src/helpers.php
+++ src/helpers.php
@@ -2,49 +2,49 @@
 
 use Illuminate\Support\Carbon;
 
-if (!function_exists('generateDateRanges')) {
-	function generateDateRanges(int $interval = 1, string $unit = 'month', ?Carbon $start = null, ?Carbon $end = null, string $format = 'Y-m-d H:i:s'): array
-	{
-		// ... 你的函数代码
-		$start = $start ?? now()->subYears(3);
-		$end = $end ?? now();
-		$ranges = [];
+if (! function_exists('generateDateRanges')) {
+    function generateDateRanges(int $interval = 1, string $unit = 'month', Carbon $start = null, Carbon $end = null, string $format = 'Y-m-d H:i:s'): array
+    {
+        // ... 你的函数代码
+        $start = $start ?? now()->subYears(3);
+        $end = $end ?? now();
+        $ranges = [];
 
-		//@phpstan-ignore-next-line
-		while ($start < $end) {
-			$next = clone $start;
+        //@phpstan-ignore-next-line
+        while ($start < $end) {
+            $next = clone $start;
 
-			switch ($unit) {
-				case 'second':
-					$next->addSeconds($interval);
-					break;
-				case 'minute':
-					$next->addMinutes($interval);
-					break;
-				case 'hour':
-					$next->addHours($interval);
-					break;
-				case 'day':
-					$next->addDays($interval);
-					break;
-				case 'month':
-				default:
-					$next->addMonths($interval);
-			}
+            switch ($unit) {
+                case 'second':
+                    $next->addSeconds($interval);
+                    break;
+                case 'minute':
+                    $next->addMinutes($interval);
+                    break;
+                case 'hour':
+                    $next->addHours($interval);
+                    break;
+                case 'day':
+                    $next->addDays($interval);
+                    break;
+                case 'month':
+                default:
+                    $next->addMonths($interval);
+            }
 
-			// Adjust the next value if it exceeds the end.
-			if ($next > $end) {
-				break;  // Exit the loop immediately
-			}
+            // Adjust the next value if it exceeds the end.
+            if ($next > $end) {
+                break;  // Exit the loop immediately
+            }
 
-			$ranges[] = [
-				'start' => $start->format($format),
-				'end'   => $next->format($format),
-			];
+            $ranges[] = [
+                'start' => $start->format($format),
+                'end' => $next->format($format),
+            ];
 
-			$start = $next;
-		}
+            $start = $next;
+        }
 
-		return $ranges;
-	}
+        return $ranges;
+    }
 }
